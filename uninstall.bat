f4c72b2c840157d0a3b62b2aadcb46949c346640
ForeverLTP <hews98@126.com>
2019-01-14 16:20:12 +0800

    完善

--- uninstall.bat
+++ uninstall.bat
@@ -1,10 +1,9 @@
 @echo off
 set JAVA_HOME=jre
+taskkill /f /im  javaw.exe  /t
 start /wait %JAVA_HOME%\bin\javaw -Xms768m -Xmx768m -XX:+HeapDumpOnOutOfMemoryError -XX:PermSize=128M -XX:MaxPermSize=128M -XX:+UseConcMarkSweepGC -jar uninstall.jar
-for /f "delims=" %%a in ('dir /s/b/a-d') do set/a b+=1
-for /f "delims=" %%a in ('dir /a-d /b /s jre') do set/a n+=1
-set/a c = %n%+3
-if%c% EQU %b%(
+if %errorlevel% == 0 (
+	del /f /q %userprofile%"\Desktop\StartNms.lnk
 	rd jre /s/q
 	del uninstall.jar
 	del uninstall.exe	
